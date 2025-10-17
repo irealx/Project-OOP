a2c2326437b9f9825f34cc40380885fb4acb66ad
HappyMood <irealx54@gmail.com>
2025-10-17 23:39:08 +0700

    add Projectile.java

--- src/entity/Monster.java
+++ src/entity/Monster.java
@@ -201,13 +201,13 @@ public class Monster extends Sprite {
         List<Monster> list = new ArrayList<>();
 
         Monster stun = new Monster(AttackType.STUN);
-        stun.setActiveLevels(2, 5);
+        stun.setActiveLevels(1, 4);
 
         Monster wrap = new Monster(AttackType.WRAP);
-        wrap.setActiveLevels(0, 3);
+        wrap.setActiveLevels(2, 5);
 
         Monster shoot = new Monster(AttackType.SHOOT);
-        shoot.setActiveLevels(1, 4);
+        shoot.setActiveLevels(0, 3);
 
         list.add(stun);
         list.add(wrap);
